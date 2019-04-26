c9b5bd6faefde8e1f39e222eb26d880b1916230f
Gian Libunao <gianlibunao@gmail.com>
2019-04-26 02:35:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   arrayHolder = []
   array.each do |number|
-    arrayHolder.push(number*number)
+    arrayHolder.push(number**2)
   end
   arrayHolder
 end
